fc75332fba7d7f948e30c21c83ecad2469b43e98
Aidan Wong <144462013+awong50@users.noreply.github.com>
2025-01-09 16:01:36 +0000

    Added starter code + working flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,19 @@
+"""
+Akasha: QianjunZhou, AidanWong, IvanGontchar, JasonChao
+SoftDev
+P02: Makers Makin' It, Act I
+2025-01-09
+Time Spent: 1
+"""
 
+from flask import Flask, render_template, redirect, session
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def landing():
+    return render_template("landing.html", logged_in = True)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
