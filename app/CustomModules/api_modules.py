901b71652e83e2f02112b40047a82591b059cd58
Aidan <aidanwonglam@gmail.com>
2025-01-12 20:12:27 -0500

    Created word generator/searching functions

---
+++ app/CustomModules/api_modules.py
@@ -0,0 +1,127 @@
+"""
+Akasha: QianjunZhou, AidanWong, IvanGontchar, JasonChao
+SoftDev
+P02: Makers Makin' It, Act I
+2025-01-09
+Time Spent: 1
+"""
+
+import urllib.request, json
+from urllib.parse import urlencode
+import random
+
+def getKey(apiName):
+    if apiName == "serpAPI":
+        apiFile = "key_serpAPI.txt"
+    elif apiName == "giphy":
+        apiFile = "key_giphyapi.txt"
+    else:
+        return "INVALID API NAME"
+
+    keyFile = "../keys/" + apiFile
+    with open(keyFile, "r") as keyFile:
+        api_key = keyFile.read().strip()
+        if api_key == "":
+            return "KEY NOT FOUND"
+        return api_key
+    
+############################# GiphyAPI #############################
+
+def getGif(tag):
+
+    APIKEY = getKey("giphy")
+
+    if APIKEY == "KEY NOT FOUND": # Easy error handling if needed
+        return 404
+    if APIKEY == "INVALID API NAME":
+        return 405
+    
+    params = {
+        "api_key": APIKEY,
+        "tag": tag,
+        "number": 30
+    }
+
+    paramString = urlencode(params)
+    url = f"https://api.giphy.com/v1/gifs/random?{paramString}"
+    headers = {'User-Agent': 'Mozilla/5.0'} 
+    request = urllib.request.Request(url, headers=headers)
+
+    try:
+        with urllib.request.urlopen(request) as response:
+            data = json.loads(response.read().decode('utf-8'))
+            if 'data' in data and data['data']:
+                return {"link": data['data']['images']["original"]["url"], "title": data['data']['title']}
+            else:
+                return "No gif found"
+    
+    except Exception as e:
+        print(f"Exception occurred: {e}")
+        return 403
+    
+############################# Datamuse #############################
+
+def getRandomSearch():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    randomLength = random.randint(3, 13)
+
+    randomLetter = random.choice(letters)
+    randomWord = randomLetter + '?' * randomLength
+
+    params = {
+        "sp": randomWord
+    }
+
+    paramString = urlencode(params)
+    url = f"https://api.datamuse.com/words?{paramString}"
+    print(url)
+    headers = {'User-Agent': 'Mozilla/5.0'} 
+    request = urllib.request.Request(url, headers=headers)
+
+    try:
+        with urllib.request.urlopen(request) as response:
+            data = json.loads(response.read().decode('utf-8'))
+            return data[random.randint(0, len(data) - 1)]["word"]
+    
+    except Exception as e:
+        print(f"Exception occurred: {e}")
+        return 403
+
+print(getRandomSearch())
+
+
+
+############################# SerpAPI #############################
+
+def getSearchVolume(keyword):
+
+    APIKEY = getKey("serpAPI")
+
+    if APIKEY == "KEY NOT FOUND": # Easy error handling if needed
+        return 404
+    if APIKEY == "INVALID API NAME":
+        return 405
+
+    params = {
+            "api_key": APIKEY,
+            "q": keyword
+        }
+    
+    paramString = urlencode(params)
+    url = f"https://serpapi.com/search?engine=google_trends&{paramString}"
+    headers = {'User-Agent': 'Mozilla/5.0'} 
+    request = urllib.request.Request(url, headers=headers)
+
+
+    try:
+        with urllib.request.urlopen(request) as response:
+            data = json.loads(response.read().decode('utf-8'))
+            if 'interest_over_time' in data and data['interest_over_time']:
+                length = len(data["interest_over_time"]["timeline_data"])
+                return data["interest_over_time"]["timeline_data"][length - 1]["values"][0]["value"]
+            else:
+                return "Error"
+    
+    except Exception as e:
+        print(f"Exception occurred: {e}")
+        return 403
---
+++ app/CustomModules/db_modules.py
@@ -0,0 +1,97 @@
+"""
+Akasha: QianjunZhou, AidanWong, IvanGontchar, JasonChao
+SoftDev
+P02: Makers Makin' It, Act I
+2025-01-09
+Time Spent: 1
+"""
+
+
+import sqlite3
+import os
+from flask import session, request
+
+# creating path to database file
+DB_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'apis.db')
+
+def database_connect():
+    conn = sqlite3.connect(DB_PATH)
+    return conn
+
+def create_database():
+    conn = database_connect()
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS logins (
+                    username TEXT NOT NULL UNIQUE PRIMARY KEY,
+                    password TEXT NOT NULL
+                );
+                ''') 
+    """
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS game_scores (
+                    username TEXT,
+                    FOREIGN KEY (username) REFERENCES logins(username) ON DELETE CASCADE,
+                    timestamp TEXT DEFAULT NULL,
+                    position INTEGER
+                );
+                ''')
+    
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    contributor TEXT,
+                    FOREIGN KEY (contributor) REFERENCES logins(username) ON DELETE CASCADE,
+                    timestamp TEXT DEFAULT NULL,
+                )
+                ''')
+    
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS individual (
+                    id INTEGER,
+                    FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE,
+                    name TEXT NOT NULL,
+                    image TEXT NOT NULL,
+                    description TEXT NOT NULL,
+                );
+                ''')
+    """
+    conn.commit()
+    conn.close()
+
+def add_user(username, password):
+    try:
+        conn = database_connect()
+        cursor = conn.cursor()
+        cursor.execute('''
+                    INSERT INTO logins (username, password)
+                    VALUES (?, ?);
+                    ''', (username, password))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        return "Username already exists"
+    finally:
+        conn.close()
+        return "User added successfully"
+        
+def login_user(username, password):
+    conn = database_connect()
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''SELECT * FROM logins WHERE username=? AND password=?;''', (username, password))
+        conn.close()
+        return "Login successful"
+    except sqlite3.IntegrityError:
+        return "Invalid username or password"
+        
+def get_game_scores():
+    conn = database_connect()
+    cursor = conn.cursor()
+    cursor.execute('''
+                SELECT * FROM game_scores
+                ORDER BY position ASC;
+                ''')
+    scores = cursor.fetchall()
+    conn.close()
+    return scores
